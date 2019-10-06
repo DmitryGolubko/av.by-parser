d828c0f149af2ea5a2a7eff702bedc6e1ca2ede1
Dmitry Golubko <golubko97@gmail.com>
2019-10-07 00:45:16 +0300

    Implemented Av.by parser

---
+++ advertisement.rb
@@ -0,0 +1,37 @@
+require 'nokogiri'
+require 'open-uri'
+require 'net/http'
+require 'pry'
+
+class Advertisement
+  attr_accessor :link, :id
+  attr_reader :make, :model, :price, :year, :odometer, :fuel_type, :engine_type, :color, :body, :transmission, :drive
+
+  def initialize(link, id)
+    @link = link
+    @id = id
+    content
+    puts '============================================================='
+    puts "#{@id} - #{@link} - #{@make} - #{@model} - #{@price} - #{@year} - #{@odometer} - #{@fuel_type} -
+          #{@engine_type} - #{@color} - #{@body} - #{@transmission} - #{@drive}"
+    puts '============================================================='
+  end
+
+  private
+
+  def content
+    request = URI(@link)
+    page = Nokogiri::HTML(open(request))
+    @make = page.css('a[@class="breadcrumb-link"]')[1].text.strip
+    @model = page.css('a[@class="breadcrumb-link"]')[2].children.last.text.strip
+    @price = page.css('span[@class="card-price-main-secondary"]').text.strip
+    @year = page.at('div[@class="card-info"] ul li dt:contains("Год выпуска")').parent.at('dd').text
+    @odometer = page.at('div[@class="card-info"] ul li dt:contains("Пробег")').parent.at('dd').text.slice!(/\d+/)
+    @fuel_type = page.at('div[@class="card-info"] ul li dt:contains("Тип топлива")').parent.at('dd').text
+    @engine_type = page.at('div[@class="card-info"] ul li dt:contains("Объем")').parent.at('dd').text.slice!(/\d+/)
+    @color = page.at('div[@class="card-info"] ul li dt:contains("Цвет")').parent.at('dd').text
+    @body = page.at('div[@class="card-info"] ul li dt:contains("Тип кузова")').parent.at('dd').text
+    @transmission = page.at('div[@class="card-info"] ul li dt:contains("Трансмиссия")').parent.at('dd').text
+    @drive = page.at('div[@class="card-info"] ul li dt:contains("Привод")')&.parent&.at('dd')&.text
+  end
+end
---
+++ parser.rb
@@ -0,0 +1,69 @@
+require 'nokogiri'
+require 'open-uri'
+require 'net/http'
+require_relative 'advertisement'
+require 'csv'
+
+
+class Parser
+  def self.get_advertisements(out_file)
+    page_number = 1
+    get_content = true
+    start_time = Time.now
+    while get_content
+      puts "Page number: #{page_number}"
+      csv = CSV.read(out_file, :headers=>true)
+      existing_ads = csv['ID']
+      advertisements = []
+      link = if page_number == 1
+               'https://cars.av.by/search?sort=price&order=asc'
+             else
+               "https://cars.av.by/search/page/#{page_number}?sort=price&order=asc"
+             end
+      request = URI(link)
+      page = Nokogiri::HTML(open(request))
+      get_content = false if page.xpath('//div[@class="listing-item-title"]').count < 25
+      page.xpath('//div[@class="listing-item-title"]').each do |element|
+        id = URI.parse(element.css('a').attribute('href').value).path.split('/').last
+        if !existing_ads.include?(id)
+          advertisements << Advertisement.new(element.css('a').attribute('href').value, id)
+        else
+          puts "#{id} is already in CSV"
+        end
+      end
+      page_number += 1
+      export_to_csv(advertisements, out_file)
+      end_time = Time.now
+      puts(convert_time(start_time, end_time))
+    end
+  end
+
+  def self.export_to_csv(advertisements, file)
+    puts 'Output to file'
+    CSV.open(file, 'a') do |csv|
+      # csv << ['ID', 'Make', 'Model', 'Price', 'Year', 'Odometer', 'Fuel type', 'Engine type', 'Color', 'Body',
+      #         'Transmission', 'Drive']
+      advertisements.each do |ad|
+        csv << [ad.id, ad.make, ad.model, ad.price, ad.year, ad.odometer, ad.fuel_type, ad.engine_type, ad.color, ad.body, ad.transmission, ad.drive]
+      end
+    end
+  end
+
+  def self.convert_time(start_time, end_time)
+    difference = end_time - start_time
+    seconds    =  difference % 60
+    difference = (difference - seconds) / 60
+    minutes    =  difference % 60
+    difference = (difference - minutes) / 60
+    hours      =  difference % 24
+    "Parsed page time: #{hours.to_i}:#{minutes.to_i}:#{seconds.to_i}"
+  end
+end
+
+start_time = Time.now
+puts "Start time: #{start_time}"
+advertisements = Parser.get_advertisements('out.csv')
+# Parser.export_to_csv(advertisements, 'out.csv')
+end_time = Time.now
+puts end_time
+puts(convert_time(start_time, end_time))
